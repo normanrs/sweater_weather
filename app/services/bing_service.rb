f1fa04e5398be5b5dbf64c29b0f7a72a80264d46
normanrs <normanrs@live.com>
2019-01-04 11:46:00 -0700

    refactored bing service

--- app/services/bing_service.rb
+++ app/services/bing_service.rb
@@ -6,15 +6,26 @@ class BingService
 
   def coords
     formatted_address = @address.split(",")
-    locality = formatted_address[0]
-    district = formatted_address[1]
-    incoming = get_coords_json(locality, district)
+    @locality = formatted_address[0]
+    @district = formatted_address[1]
+    incoming = get_coords_json
     incoming[:resourceSets][0][:resources][0][:point][:coordinates]
   end
 
-  def get_coords_json(locality, district)
-    @response ||= Faraday.get("http://dev.virtualearth.net/REST/v1/Locations?locality=#{locality}&adminDistrict=#{district}&countryRegion=United States&key=#{ENV["bing_key"]}")
-    @parsed ||= JSON.parse(@response.body, symbolize_names: true)
+  def get_coords_json
+    url = "/REST/v1/Locations"
+    response = conn.get(url)
+    @parsed ||= JSON.parse(response.body, symbolize_names: true)
   end
 
 end
+
+	def conn
+		Faraday.new(url: "http://dev.virtualearth.net") do |faraday|
+      faraday.params['locality'] = @locality
+      faraday.params['adminDistrict'] = @district
+      faraday.params['key'] = ENV["bing_key"]
+			faraday.adapter Faraday.default_adapter
+		end
+
+	end
