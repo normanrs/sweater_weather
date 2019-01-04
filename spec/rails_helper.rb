7e1705e0dff7653bcb82cd207a412abfd06eca41
normanrs <normanrs@live.com>
2019-01-04 08:59:22 -0700

    tightened testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start
+ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
