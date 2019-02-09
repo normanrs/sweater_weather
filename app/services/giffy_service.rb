56140566186f913d7affa2045ca994c302e31876
normanrs <normanrs@live.com>
2019-02-09 13:25:51 -0700

    Fixed gif search lastime

--- app/services/giffy_service.rb
+++ app/services/giffy_service.rb
@@ -8,6 +8,7 @@ class GiffyService
  def get_gif_url(text)
   all_data = get_json(text)
   all_data[:data][0][:embed_url]
+
  end
 
  def get_json(text)
@@ -20,7 +21,7 @@ class GiffyService
   words = text.downcase.split(/\W+/)
   keywords = ["autumn", "blast", "blizzard", "bolt", "breeze", "brisk", "calm", "chill", "chilly", "clear", "cloud", "cloudy", "cold", "cool", "dawn", "day", "daylight", "deluge", "dense", "dew", "dewy", "downpour", "drizzling", "drizzle", "droplet", "dry", "dust", "easterly", "fall", "falling", "flash", "flood", "flooding", "flurries", "flurry", "fog", "foggy", "fresh", "freeze", "freezing", "frost", "gale", "gentle", "gust", "gusts", "hail", "haze", "hazy", "heat", "heavy", "humid", "humidity", "hurricane", "ice", "icing", "icicle", "icy", "light", "lightning", "melt", "melting", "mild", "mist", "misty", "misting", "nightfall", "overcast", "patchy", "piercing", "pouring", "precipitation", "rain", "rainbow", "raindrop", "rainfall", "rainstorm", "raining", "roads", "sandstorm", "season", "shower", "sky", "sleet", "slick", "slippery", "smog", "smoggy", "snow", "snowbank", "snowdrift", "snowfall", "snowflake", "snowstorm", "spring", "springtime", "steady", "storm", "strong", "summer", "summertime", "sun", "sunbeams", "sunlight", "sunrays", "sunrise", "sunset", "sunshine", "thaw", "thunder", "thunderbolt", "thunderclap", "thundercloud", "thunderstorm", "tornado", "tropical", "twilight", "typhoon", "warm", "westerly", "wet", "whirlwind", "wind", "windy", "windstorm", "winter", "wintertime" ]
   new_array = words.select { |e| keywords.include?(e) }
-  new_array += ["nature", "@signwithrobert"]
+  new_array += ["nature", "sign", "language"]
   new_array.join(" ")
  end
 
